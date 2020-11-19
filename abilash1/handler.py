6aa443d6e42832973ceaadbcc312054d886ebead
abilashthota <74603150+abilashthota@users.noreply.github.com>
2020-11-19 12:37:57 +0530

    Add files via upload

---
+++ abilash1/handler.py
@@ -0,0 +1,11 @@
+import sys
+import json
+def endpoint(event, context):
+    body = {
+        "message": "Hello world"
+    }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
